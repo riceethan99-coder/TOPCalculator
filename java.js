2961f029ebca6cf357a3c12ade6c9f31f8597af3
Ethan Rice <riceethan99@gmail.com>
2026-04-13 17:34:59 +0100

    Basic functionailty added
    All 4 operators work for 1 round. When being asked to then follow on
    there is bugs as num2 is just concatenated with the digit buttons being
    pressed.

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
-let num1 = 0 
-let num2 = 0
-let operator  
+let num1 = ""   
+let num2 = ""
+let operator = ""
 
 function add(num1, num2) {
     return num1 + num2
@@ -32,3 +32,33 @@ function operate(num1, num2, operator) {
     }
 }
 
+let digitBtn = document.querySelectorAll(".digitBtn")
+let displayText = document.querySelector(".display")
+let operatorBtn = document.querySelectorAll(".operatorBtn")
+let equalBtn = document.querySelector(".equalBtn")
+
+
+digitBtn.forEach(button => {
+    button.addEventListener("click", function(event) {
+        if(num1 === "" || operator === "") {
+        num1 = num1 + event.target.textContent 
+        displayText.textContent = num1
+        } else {
+            num2 = num2 + event.target.textContent
+            displayText.textContent = num2
+        }
+        
+    })
+})
+
+
+operatorBtn.forEach(button => {
+    button.addEventListener("click", function(event){
+        operator = event.target.textContent
+    })
+})
+
+equalBtn.addEventListener("click", function() {
+    let result = operate(Number(num1), Number(num2), operator)
+    displayText.textContent = result 
+})
